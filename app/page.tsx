f7781881b178a1a3740471cd02956119306048e6
Sinisa <sinisha.stojanovic@gmail.com>
2025-02-03 11:03:40 +0100

    Root page layout and reusable LoginButton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import LoginButton from "@/components/auth/LoginButton";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button size={"lg"} variant={"outline"}>
-      Click me
-    </Button>
+    <main
+      className={
+        "flex flex-col items-center justify-center h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
+      }
+    >
+      <div className={"space-y-6 text-center"}>
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className={"text-white text-lg"}>A simple authentication service</p>
+
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
