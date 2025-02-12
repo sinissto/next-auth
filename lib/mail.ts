81b8a350d0c7560ec1e53ec64c60ea329229c4d2
Sinisa <sinisha.stojanovic@gmail.com>
2025-02-12 19:22:08 +0100

    2FA Send mail with 2FA code function

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,7 +1,15 @@
 import { Resend } from "resend";
-import { reset } from "next/dist/lib/picocolors";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code: ${token}</p>`,
+  });
+};
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
