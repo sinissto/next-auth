b0697eb9a0fb146cd065d85f4cdb4be2a2506966
Sinisa <sinisha.stojanovic@gmail.com>
2025-02-12 19:19:11 +0100

    2FA Generate token function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,31 @@ import { v4 as uuidv4 } from "uuid";
 import { getVerificationTokenByEmail } from "@/data/verificationToken";
 import { db } from "@/lib/db";
 import { getPasswordResetTokenByEmail } from "@/data/passwordResetToken";
+import crypto from "crypto";
+import { getTwoFactorTokenByEmail } from "@/data/twoFactorToken";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
